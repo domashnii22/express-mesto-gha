420a389c04540a55af6d16e15680fc298b780601
Aleksey Guskov <domashnii.al@yandex.ru>
2023-08-23 12:18:02 +0300

    ревью-1.1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// eslint-disable-next-line import/no-extraneous-dependencies
 const helmet = require("helmet");
 
 const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/testdb" } =
