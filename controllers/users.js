fac2536e9845d8cf7f42d753988e6e7d304322c2
Aleksey Guskov <domashnii.al@yandex.ru>
2023-08-23 21:25:35 +0300

    ревью-1.2

--- controllers/users.js
+++ controllers/users.js
@@ -48,30 +48,28 @@ module.exports.editUserData = (req, res) => {
 };
 
 module.exports.editUserAvatar = (req, res) => {
-  if (req.user._id) {
-    User.findByIdAndUpdate(
-      req.user._id,
-      { avatar: req.body.avatar },
-      { new: "true", runValidators: true },
-    )
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err.name === "ValidationError") {
-          res.status(400).send({ message: err.message });
-        } else {
-          res
-            .status(404)
-            .send({ message: "Пользователь по указанному _id не найден" });
-        }
-      });
-  } else {
-    res.status(500).send({ message: "На сервере произошла ошибка" });
-  }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: req.body.avatar },
+    { new: "true", runValidators: true },
+  )
+    .orFail(new Error("NotValidId"))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: err.message });
+      } else if (err.message === "NotValidId") {
+        res
+          .status(404)
+          .send({ message: "Пользователь по указанному _id не найден" });
+      } else {
+        res.status(500).send({ message: "На сервере произошла ошибка" });
+      }
+    });
 };
 
 module.exports.addUser = (req, res) => {
   const { name, about, avatar } = req.body;
-
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
