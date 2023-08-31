709cc6bc9c607bbce8240036fd65de10387c1992
Aleksey Guskov <domashnii.al@yandex.ru>
2023-08-31 13:04:21 +0300

    14_proj_1.04

--- controllers/cards.js
+++ controllers/cards.js
@@ -70,7 +70,7 @@ module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
-    { new: true }
+    { new: true },
   )
     .orFail(new Error("NotValidCardId"))
     .populate(["owner", "likes"])
@@ -92,7 +92,7 @@ module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
-    { new: true }
+    { new: true },
   )
     .orFail(new Error("NotValidCardId"))
     .populate(["owner", "likes"])
